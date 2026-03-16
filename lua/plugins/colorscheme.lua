81083064e809dbfc6d6b9e158862c85113e354db
christiandarvs <christiandarvin4@gmail.com>
2026-03-17 06:19:56 +0800

    edit colorscheme, plugins, snippets, etc

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,29 @@
 return {
-  -- 1. Install and configure the catppuccin plugin
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- Make sure to load this before all the other start plugins
-    opts = {
-      flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
-      transparent_background = false,
-    },
-  },
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        transparent = true,
+        theme = "dragon", -- wave, dragon, lotus
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            -- Explicitly style the popup menu to match the Dragon theme
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- Popup menu background
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 }, -- Selected item background
+            PmenuSbar = { bg = theme.ui.bg_m1 }, -- Scrollbar background
+            PmenuThumb = { bg = theme.ui.bg_p2 }, -- Scrollbar thumb
 
-  -- 2. Tell LazyVim to use catppuccin as the default colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+            -- Optional: If you want floating windows (like hover docs) to match
+            NormalFloat = { bg = theme.ui.bg_p1 },
+            FloatBorder = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+          }
+        end,
+      })
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
   },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,20 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs",
   opts = {
     ensure_installed = {
       "python",
       "lua",
       "cpp",
       "go",
+    },
+    indent = {
+      enable = true,
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
     },
   },
 }
