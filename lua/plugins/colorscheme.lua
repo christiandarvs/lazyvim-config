dca49718b218166206caa33ba383c34a55e761f2
christiandarvs <christiandarvin4@gmail.com>
2026-04-13 14:05:43 +0800

    replace colorscheme with oxocarbon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,65 +1,24 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- Load this before all other start plugins
-    opts = {
-      flavour = "mocha", -- Default flavour: latte, frappe, macchiato, mocha
-      background = { -- Toggle variants based on terminal background
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        rainbow_delimiters = true, -- Added this back in for your brackets!
-        mini = {
-          enabled = true,
-          indentscope_color = "",
-        },
-        -- Enable C++ specific highlights if needed
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-          },
-          inlay_hints = {
-            background = true,
-          },
-        },
-      },
-    },
+    "nyoom-engineering/oxocarbon.nvim",
+    priority = 1000,
   },
 
-  -- Configure LazyVim to load the correct Catppuccin flavor based on time
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = function()
         local hour = tonumber(os.date("%H"))
 
-        -- Daytime: 7 AM to 5:59 PM (Latte)
+        -- Daytime: 7 AM to 5:59 PM (Light Mode)
         if hour >= 7 and hour < 18 then
           vim.o.background = "light"
-          vim.cmd.colorscheme("catppuccin-latte")
-        -- Nighttime: 6 PM to 6:59 AM (Mocha)
+        -- Nighttime: 6 PM to 6:59 AM (Dark Mode)
         else
           vim.o.background = "dark"
-          vim.cmd.colorscheme("catppuccin-mocha")
         end
+
+        vim.cmd.colorscheme("oxocarbon")
       end,
     },
   },
